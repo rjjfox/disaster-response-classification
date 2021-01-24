c3c7bf497062f22a10e68d2f189b43eee77dce1f
rjjfox <ryanfox212@gmail.com>
2021-01-24 15:28:46 +0000

    Add try/except to allow for cloud deployment
    To enable cloud deployment from PythonAnywhere

--- app/run.py
+++ app/run.py
@@ -7,11 +7,11 @@ from nltk.tokenize import word_tokenize
 
 from flask import Flask
 from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar
+import plotly.graph_objects as go
 import joblib
 from sqlalchemy import create_engine
 
-# from app.plots import return_figures
+from plots import return_figures
 
 
 app = Flask(__name__)
@@ -29,74 +29,15 @@ def tokenize(text):
     return clean_tokens
 
 
-def return_figures(df):
-    """Create Plotly figures object"""
-
-    genres_plot = []
-    genres_plot.append(
-        go.Bar(
-            x=df.genre.value_counts().index.tolist(),
-            y=df.genre.value_counts().values.tolist(),
-            hoverinfo="skip",
-            marker_color="#BB764B",
-        )
-    )
-
-    genres_layout = dict(
-        {
-            "title": {
-                "text": "<b>Majority of messages come directly or via "
-                "news</b><br>Small percentage of the data comes"
-                " from social",
-                "font": {"family": "Roboto", "size": 16},
-            },
-        }
-    )
-
-    label_tallies = df.iloc[:, 4:].sum().sort_values(ascending=True)
-    label_plot = []
-    label_plot.append(
-        go.Bar(
-            x=label_tallies.values.tolist(),
-            y=label_tallies.index.tolist(),
-            orientation="h",
-            marker_color="#BB764B",
-        )
-    )
-
-    label_layout = dict(
-        {
-            "title": {
-                "text": "<b>Not all messages are relevant"
-                "</b><br>75% of messages labelled <i>related</i> to mean"
-                " relevant",
-                "font": {"family": "Roboto", "size": 16},
-            },
-            "margin": {
-                "pad": 10,
-                "l": 140,
-                "r": 40,
-                "t": 80,
-                "b": 40,
-            },
-            "hoverlabel": {
-                "font_size": 18,
-                "font_family": "Roboto",
-            },
-            "yaxis": {"dtick": 1},
-        }
-    )
-
-    figures = []
-    figures.append(dict(data=genres_plot, layout=genres_layout))
-    figures.append(dict(data=label_plot, layout=label_layout))
-
-    return figures
-
-
 # load data
-engine = create_engine("sqlite:///data/DisasterResponse.db")
-df = pd.read_sql_table("messages", engine)
+try:
+    engine = create_engine("sqlite:///data/DisasterResponse.db")
+    df = pd.read_sql_table("messages", engine)
+except:
+    engine = create_engine(
+        "sqlite:////home/ryanfox212/disaster-response-classification/data/DisasterResponse.db"
+    )
+    df = pd.read_sql_table("messages", engine)
 
 # load model
 model = joblib.load("models/classifier.pkl")
@@ -118,7 +59,10 @@ def index():
 
     # render web page with plotly graphs
     return render_template(
-        "master.html", ids=ids, graphJSON=graphJSON, sample_messages=sample_messages
+        "master.html",
+        ids=ids,
+        graphJSON=graphJSON,
+        sample_messages=sample_messages,
     )
 
 
