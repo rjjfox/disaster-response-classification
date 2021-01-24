0ba2bea068507c6c5c41f2a9de6dbde2de95a131
rjjfox <ryanfox212@gmail.com>
2021-01-24 14:39:09 +0000

    Restructure files to allow for deployment

--- app/run.py
+++ app/run.py
@@ -11,7 +11,7 @@ from plotly.graph_objs import Bar
 import joblib
 from sqlalchemy import create_engine
 
-from plots import return_figures
+# from app.plots import return_figures
 
 
 app = Flask(__name__)
@@ -27,6 +27,71 @@ def tokenize(text):
         clean_tokens.append(clean_tok)
 
     return clean_tokens
+
+
+def return_figures(df):
+    """Create Plotly figures object"""
+
+    genres_plot = []
+    genres_plot.append(
+        go.Bar(
+            x=df.genre.value_counts().index.tolist(),
+            y=df.genre.value_counts().values.tolist(),
+            hoverinfo="skip",
+            marker_color="#BB764B",
+        )
+    )
+
+    genres_layout = dict(
+        {
+            "title": {
+                "text": "<b>Majority of messages come directly or via "
+                "news</b><br>Small percentage of the data comes"
+                " from social",
+                "font": {"family": "Roboto", "size": 16},
+            },
+        }
+    )
+
+    label_tallies = df.iloc[:, 4:].sum().sort_values(ascending=True)
+    label_plot = []
+    label_plot.append(
+        go.Bar(
+            x=label_tallies.values.tolist(),
+            y=label_tallies.index.tolist(),
+            orientation="h",
+            marker_color="#BB764B",
+        )
+    )
+
+    label_layout = dict(
+        {
+            "title": {
+                "text": "<b>Not all messages are relevant"
+                "</b><br>75% of messages labelled <i>related</i> to mean"
+                " relevant",
+                "font": {"family": "Roboto", "size": 16},
+            },
+            "margin": {
+                "pad": 10,
+                "l": 140,
+                "r": 40,
+                "t": 80,
+                "b": 40,
+            },
+            "hoverlabel": {
+                "font_size": 18,
+                "font_family": "Roboto",
+            },
+            "yaxis": {"dtick": 1},
+        }
+    )
+
+    figures = []
+    figures.append(dict(data=genres_plot, layout=genres_layout))
+    figures.append(dict(data=label_plot, layout=label_layout))
+
+    return figures
 
 
 # load data
